08629da563279a4bc28aca969f5f5b969556240e
stick2000 <martin.hockey@civil.lmco.com>
2017-01-24 16:20:01 +0000

    more

--- CallingScript.ps1
+++ CallingScript.ps1
@@ -24,7 +24,6 @@ Login-AzureRmAccount
 # something unique for you. Leave the last two characters in each.
 # $URI       = 'https://raw.githubusercontent.com/GoateePFE/AzureRM/master/active-directory-new-domain-with-data/azuredeploy.json'
 $URI       = 'https://raw.githubusercontent.com/stick2000/ad-lab/master/azuredeploy.json'
-$URIT      = 'https://raw.githubusercontent.com/stick2000/ad-lab/master/azuredeploy.parameters.json'
 $Location  = 'West Europe'
 $rgname    = 'stick2000rg'
 $saname    = 'stick2000sa'     # Lowercase required
@@ -48,7 +47,6 @@ $MyParams = @{
 # Splat the parameters on New-AzureRmResourceGroupDeployment  
 $SplatParams = @{
     TemplateUri             = $URI
-    TemplateParameterUri    =  
     ResourceGroupName       = $rgname 
     TemplateParameterObject = $MyParams
     Name                    = 'Stick'
